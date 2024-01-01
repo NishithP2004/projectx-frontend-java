01b178d31d0d2b14832bf9243cfee72bd3a70ee9
Nishith P <nishithp11@gmail.com>
2024-01-02 01:04:07 +0530

    Initial Commit

---
+++ src/main/java/com/noogleai/projectx/Projectx.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.noogleai.projectx;
+
+/**
+ *
+ * @author Nishith P
+ */
+public class Projectx {
+
+    public static void main(String[] args) {
+        Main mainFrame = new Main();
+        mainFrame.setVisible(true);
+    }
+}
